256938ab0cc38118419dd8a6d58ae44c48df0da7
yvjennig <yvonne.jenniges@awi.de>
2025-03-14 17:45:14 +0100

    add function to plot embedding

--- visualisation/plotting.py
+++ visualisation/plotting.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
+import pandas as pd
 
 
+# Potentially remove this one again?
 def plot_each_depth_level(df, color_label="water", save_as="output/grid_cells.png"):
     # Show grid cells for every depth layer
     depths = df["LEV_M"].unique()
@@ -36,3 +38,50 @@ def plot_boxplots(df, parameters, save_as):
 
 def plot_histograms(df, save_as=None):
     pass
+
+
+def plot_embedding(embedding, color_label=None, alpha=0.08, size=2, save_as=None, figsize=(6, 6), fontsize=8,
+                   ticklabelsize=8, dpi=1000):
+    """ Plot a 2d or 3d embedding. (It can be a pandas.DataFrame or a numpy.array.) """
+    # determine x, y, z, data and colour
+    if isinstance(embedding, pd.DataFrame):
+        x = embedding["e0"]
+        y = embedding["e1"]
+        z = embedding["e2"] if "e2" in embedding.columns else None
+        c = embedding[color_label] if color_label is not None else None
+    else:
+        x = embedding[:, 0]
+        y = embedding[:, 1]
+        z = embedding[:, 2] if embedding.shape[1] == 3 else None
+        c = color_label
+
+        # plot
+    fig = plt.figure(figsize=figsize)
+
+    # 3d
+    if z is not None:
+        ax = fig.add_subplot(projection='3d')
+        ax.set_zlabel("Z-axis")
+        if c is None:
+            ax.scatter(x, y, z, alpha=alpha, s=size, marker=".")
+        else:
+            ax.scatter(x, y, z, alpha=alpha, c=c, s=size, marker=".")
+
+        plt.subplots_adjust(left=-0.05, right=0.9, top=1.1, bottom=0)
+    # 2d
+    else:
+        if c is None:
+            plt.scatter(x, y, alpha=alpha, s=size, marker=".")
+        else:
+            plt.scatter(x, y, alpha=alpha, c=c, s=size, marker=".")
+
+    plt.xlabel("X-axis", fontsize=fontsize)
+    plt.ylabel("Y-axis", fontsize=fontsize)
+    ax.tick_params(axis='x', labelsize=ticklabelsize)  # , pad=tick_padding)
+    ax.tick_params(axis='y', labelsize=ticklabelsize)  # , pad=tick_padding)
+    ax.tick_params(axis='z', labelsize=ticklabelsize)  # , pad=tick_padding)
+    plt.tight_layout()
+    plt.subplots_adjust(left=-0.01, right=0.92, top=1.1, bottom=0)
+    if save_as:
+        plt.savefig(save_as, dpi=dpi)
+    plt.show()
