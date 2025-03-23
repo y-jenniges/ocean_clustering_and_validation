57bd6362c06dd4cb16aeca299c69be86f8a1ced0
yvjennig <yvonne.jenniges@awi.de>
2025-03-23 10:28:40 +0100

    add new kmeans hyps derived from new experiments

--- hyp_config.py
+++ hyp_config.py
@@ -3,10 +3,12 @@
 umap_hyps = {"n_neighbors": 20, "min_dist": 0.0, "n_components": 3}
 
 # Final hyperparameters for KMeans on original data
-kmeans_original_hyps = {"n_clusters": 2, "n_init": "auto"}
+# kmeans_original_hyps = {"n_clusters": 2, "n_init": "auto"}
+kmeans_original_hyps = {"n_clusters": 3, "n_init": "auto"}
 
 # Final hyperparameters for KMeans on embedded data
-kmeans_umap_hyps = {"n_clusters": 10}
+# kmeans_umap_hyps = {"n_clusters": 10, "n_init": "auto"}
+kmeans_umap_hyps = {"n_clusters": 3, "n_init": "auto"}
 
 # Final hyperparameters for Ward on original data
 ward_original_hyps = {"n_clusters": 2, "linkage": "ward", "distance_threshold": None}  # , "compute_distances": True}
