ac1bd76a829fca5a97b3b6efc74fe8df0619f937
yvjennig <yvonne.jenniges@awi.de>
2025-04-11 10:26:39 +0200

    tidy up

--- visualisation/interactive_embedding.py
+++ visualisation/interactive_embedding.py
@@ -1,22 +1,23 @@
 import plotly.graph_objects as go
 import plotly.io as pio
-import numpy as np
 import pandas as pd
+
 import config
+
 
 # Open plot in browser
 pio.renderers.default = "browser"
 
 # Load an embedding
-i = 33
-filename = f"{config.output_dir_uncertainty}umap_dbscan_{i}.csv"
+filename = f"{config.output_dir_nemi}volume_nemi_iteration84.csv"
 df = pd.read_csv(filename)
+df = df[df.label != 0]
 
 # Create 3D scatter plot
 fig = go.Figure(data=[go.Scatter3d(
     x=df["e0"], y=df["e1"], z=df["e2"],
     mode='markers',
-    marker=dict(size=1, opacity=0.8)  # color=z, colorscale='Viridis',
+    marker=dict(size=1, opacity=0.8, color=df["label_color"])
 )])
 
 # Customize layout
