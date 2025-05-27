5e348753e7e2d39bb1b66c9a79348da770a27fb0
yvjennig <yvonne.jenniges@awi.de>
2025-05-27 15:02:55 +0200

    Update umap neighbors

--- config.py
+++ config.py
@@ -58,7 +58,7 @@ bathymetry_path = "../../data/bathymetry/gebco_2022_sub_ice_topo/GEBCO_2022_sub_
 n_iterations = 10
 # umap_hyps = {"n_neighbors": 20, "min_dist": 0.0, "n_components": 3}
 # preprocessings = {"minmax": [MinMaxScaler], "minmax_umap": [MinMaxScaler, UMAP(**umap_hyps)]}
-umap_hyps = {"n_neighbors": 50, "min_dist": 0.8, "n_components": 3, "metric": "euclidean"}
+umap_hyps = {"n_neighbors": 150, "min_dist": 0.8, "n_components": 3, "metric": "euclidean"}
 preprocessings = {"robust": [RobustScaler], "robust_umap": [RobustScaler, UMAP(**umap_hyps)]}
 algorithms_and_hyps = {"kmeans": (KMeans, {"n_clusters": list(range(2, 16)) + [20, 30, 40, 50, 60],
                                            "n_init": ["auto"]}),
