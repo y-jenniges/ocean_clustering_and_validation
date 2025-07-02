6c135e3db66251a044e331ec6a93dd34fe203615
yvjennig <yvonne.jenniges@awi.de>
2025-07-02 18:07:54 +0200

    Update final hyperparameter selection

--- hyp_config.py
+++ hyp_config.py
@@ -3,13 +3,13 @@
 kmeans_original_hyps = {"n_clusters": 2, "n_init": "auto"}
 
 # Final hyperparameters for KMeans on embedded data
-kmeans_umap_hyps = {"n_clusters": 10, "n_init": "auto"}
+kmeans_umap_hyps = {"n_clusters": 8, "n_init": "auto"}
 
 # Final hyperparameters for Ward on original data
 ward_original_hyps = {"n_clusters": 2, "linkage": "ward"}
 
 # Final hyperparameters for Ward on embedded data
-ward_umap_hyps = {"n_clusters": 23, "linkage": "ward"}
+ward_umap_hyps = {"n_clusters": 25, "linkage": "ward"}
 
 # Final hyperparameters for DBSCAN on original data
 dbscan_original_hyps = {"eps": 0.11949153, "min_samples": 11}
@@ -21,12 +21,4 @@ dbscan_umap_hyps = {"eps": 0.10661017, "min_samples": 4}
 spatial_original_hyps = {"n_clusters": 2, "linkage": "ward"}
 
 # Final hyperparameters for Spatial Ward on embedded data
-spatial_umap_hyps = {"n_clusters": 12, "linkage": "ward"}
-
-
-# CH: 3
-# DB: 2
-# SH: 2
-# k-DBCV: 60
-# CVNN Hal:
-# CDR: 60
+spatial_umap_hyps = {"n_clusters": 10, "linkage": "ward"}
